a3018906b419c7e1df69db2c8c2be9db1baa257d
eVillesseche <ethan.Villesseche@epitech.eu>
2021-08-31 13:49:10 +0200

    fix responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,20 @@ module.exports = {
 
       'max-sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
+      '2xl': {'min': '1535px'},
+      // => @media (min-width: 1535px) { ... }
+
+      'xl': {'min': '1279px'},
+      // => @media (min-width: 1279px) { ... }
+
+      'lg': {'min': '1023px'},
+      // => @media (min-width: 1023px) { ... }
+
+      'md': {'min': '767px'},
+      // => @media (min-width: 767px) { ... }
+
+      'sm': {'min': '639px'},
+      // => @media (min-width: 639px) { ... }
     },
     extend: {},
   },
